2452f8a7ea0641bb8530036191ea76cfe0eaa3e9
Michael Bruce Allen <CodeAmend@Gmail.Com>
2016-08-23 14:40:13 -0400

    updated serializer.py with ModelSerializer | shortcut class

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
+from snippets.models import Snippet
+
+
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+    
+"""
+from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
-
-
-class SnippetSerializer(serializers.Serializer):
+        class SnippetSerializer(serializers.Serializer):
     pk = serializers.IntegerField(read_only=True)
     title = serializers.CharField(required=False, allow_blank=True, max_length=100)
     code = serializers.CharField(style={'base_template': 'textarea.html'})
@@ -11,15 +19,15 @@ class SnippetSerializer(serializers.Serializer):
     style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
 
     def create(self, validated_data):
-        """
-        Create and return a new `Snippet` instance, given the validated data.
-        """
+
+        # Create and return a new `Snippet` instance, given the validated data.
+
         return Snippet.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        """
-        Update and return an existing `Snippet` instance, given the validated data.
-        """
+
+        # Update and return an existing `Snippet` instance, given the validated data.
+
         instance.title = validated_data.get('title', instance.title)
         instance.code = validated_data.get('code', instance.code)
         instance.linenos = validated_data.get('linenos', instance.linenos)
@@ -27,3 +35,5 @@ class SnippetSerializer(serializers.Serializer):
         instance.style = validated_data.get('style', instance.style)
         instance.save()
         return instance
+"""
+
